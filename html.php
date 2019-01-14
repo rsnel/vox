70823255c8a065122f4acee309a5a819cf01d224
Rik Snel <rik@snel.it>
2019-01-14 09:05:43 +0100

    layout van klassenboek overgenomen

--- html.php
+++ html.php
@@ -27,15 +27,15 @@ function html_start() {
 <li id="ppl_id">ppl_id=<? echo(($GLOBALS['session_state']['ppl_id'])?$GLOBALS['session_state']['ppl_id']:'<i>NULL</i>'); ?></li>
 <? if (check_su()) { ?><li>switched user naar <?=db_single_field("SELECT ppl_login FROM $voxdb.ppl WHERE ppl_id = ?", $GLOBALS['session_state']['ppl_id']); ?></li><? } ?>
 </ul>
-<ul>
+<ul id="menu">
 <? if (check_logged_in()) { ?>
 <li><form method="POST" action="do_logout.php?session_guid=<? echo($GLOBALS['session_guid']); ?>">ingelogd als <?=$GLOBALS['session_state']['auth_user']?> <input type="submit" value="logout"></form></li>
-<? if (check_su()) { ?><li><form method="POST" action="do_su.php?session_guid=<? echo($GLOBALS['session_guid']); ?>">su identiteit <?=db_single_field("SELECT ppl_login FROM $voxdb.ppl WHERE ppl_id = ?", $GLOBALS['session_state']['ppl_id'])?><input type="hidden" name="username" value="<?=$GLOBALS['session_state']['auth_user']?>"> <input type="submit" value="switch terug"></form></li>
+<? if (check_su()) { ?><li><form method="POST" action="do_su.php?session_guid=<? echo($GLOBALS['session_guid']); ?>">suid <?=db_single_field("SELECT ppl_login FROM $voxdb.ppl WHERE ppl_id = ?", $GLOBALS['session_state']['ppl_id'])?><input type="hidden" name="username" value="<?=$GLOBALS['session_state']['auth_user']?>"> <input type="submit" value="switch terug"></form></li>
 <? } else if (check_staff()) { ?><li><form method="POST" action="do_su.php?session_guid=<? echo($GLOBALS['session_guid']); ?>"><input type="text" name="username" value=""><input type="submit" value="switch user"></form>
 </li><? }
 if (!preg_match("/edit_password.php/", $_SERVER['PHP_SELF'])) {
 ?>
-<li><a href="edit_password.php?session_guid=<?=$GLOBALS['session_guid']?>">wachtwoord wijzigen</a></li>
+<li><a href="edit_password.php?session_guid=<?=$GLOBALS['session_guid']?>">wijzig ww.</a></li>
 <? if (!preg_match("/rooster.php/", $_SERVER['PHP_SELF']) && !preg_match("/klassenlijst.php/", $_SERVER['PHP_SELF'])) { ?>
 <li><a href="rooster.php?session_guid=<?=$GLOBALS['session_guid']?>">rooster</a></li>
 <? } ?>
@@ -50,34 +50,36 @@ if (!preg_match("/edit_password.php/", $_SERVER['PHP_SELF'])) {
 <li><a href="index.php?session_guid=<?=$GLOBALS['session_guid']?>">home</a></li>
 <? } ?>
 <? if (check_permission('ACCOUNT') && !preg_match("/account.php/", $_SERVER['PHP_SELF'])) { ?>
-<li><a href="account.php?session_guid=<?=$GLOBALS['session_guid']?>">accountbeheer</a></li>
+<li><a href="account.php?session_guid=<?=$GLOBALS['session_guid']?>">accounts</a></li>
 <? } ?>
 <? if (check_permission('PERMISSIONS') && !preg_match("/permissions?.php/", $_SERVER['PHP_SELF'])) { ?>
-<li><a href="permissions.php?session_guid=<?=$GLOBALS['session_guid']?>">permissionsbeheer</a></li>
+<li><a href="permissions.php?session_guid=<?=$GLOBALS['session_guid']?>">pb</a></li>
 <? } ?>
 <? if (check_permission('CONFIGS') && !preg_match("/(configs.php|edit_config.php)/", $_SERVER['PHP_SELF'])) { ?>
-<li><a href="configs.php?session_guid=<?=$GLOBALS['session_guid']?>">configbeheer</a></li>
+<li><a href="configs.php?session_guid=<?=$GLOBALS['session_guid']?>">cb</a></li>
 <? } ?>
 <? if (check_permission('TAGBEHEER') && !preg_match("/tags.php/", $_SERVER['PHP_SELF'])) { ?>
-<li><a href="tags.php?session_guid=<?=$GLOBALS['session_guid']?>">tagbeheer</a></li>
+<li><a href="tags.php?session_guid=<?=$GLOBALS['session_guid']?>">tags</a></li>
 <? } ?>
 <? if (check_permission('WEEKBEHEER') && !preg_match("/weken.php/", $_SERVER['PHP_SELF']) && !preg_match("/week_ops.php/", $_SERVER['PHP_SELF'])) { ?>
-<li><a href="weken.php?session_guid=<?=$GLOBALS['session_guid']?>">weekbeheer</a></li>
+<li><a href="weken.php?session_guid=<?=$GLOBALS['session_guid']?>">weken</a></li>
 <? } ?>
 </ul>
 <? if ($GLOBALS['session_state']['success_msg']) { ?>
-<div id="errormsg"><span class="textual">success:</span>
+<div id="successmsg"><span class="textual">success:</span>
 <? echo(($GLOBALS['session_state']['success_msg'])?$GLOBALS['session_state']['success_msg']:'<i>NULL</i>'); ?></div>
 <?	$GLOBALS['session_state']['success_msg'] = NULL;
 } ?>
 <? if ($GLOBALS['session_state']['error_msg']) { ?>
-<div id="successmsg"><span class="textual">error:</span>
+<div id="errormsg"><span class="textual">error:</span>
 <? echo(($GLOBALS['session_state']['error_msg'])?$GLOBALS['session_state']['error_msg']:'<i>NULL</i>'); ?></div>
 <?  $GLOBALS['session_state']['error_msg'] = NULL;
 	}
+?><div style="clear: both"><?
 }
 
 function html_end() { ?>
+</div>
 <div id="footer">
 VOX Inschrijfsysteem &copy; 2018-2019 Rik Snel &lt;rik@snel.it&gt;.<br>
 Released as <a href="http://www.gnu.org/philosophy/free-sw.html">free software</a> without warranties under <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU AGPL v3</a>.<br>
