c0f5c19cebc60d0eeb136b3c3df9949fc2e21686
Rik Snel <rik@snel.it>
2019-01-17 07:44:05 +0100

    update accountscherm

--- account.php
+++ account.php
@@ -6,17 +6,52 @@ require('html.php');
 enforce_permission('ACCOUNT');
 
 $res = db_query(<<<EOQ
-SELECT ppl_login login,
-	CONCAT(ppl_forename, ' ', ppl_prefix, ' ', ppl_surname) naam, 
+SELECT ppl_login login, CONCAT(ppl_forename, ' ', ppl_prefix, ' ', ppl_surname, IFNULL(CONCAT(' (', tags, ')'), '')) naam,
 	CONCAT('<input type="checkbox"', 
 		IF(password_hash IS NULL, ' checked', ''),
-		' value="', ppl_id, '-', IFNULL(log_id, 'NULL'),
-		'" name="pplpwlog[]">') `genereer wachtwoord`
-FROM $voxdb.ppl LEFT JOIN passwords ON passwords.auth_user = ppl_login
-ORDER BY ppl_type, ppl_surname, ppl_forename, ppl_prefix
+		' value="', ppl.ppl_id, '-', IFNULL(log_id, 'NULL'),
+		'" name="pplpwlog[]">') `genereer wachtwoord`,
+		IFNULL(last_activity, 'nog nooit ingelogd') `laatste activiteit`
+
+FROM $voxdb.ppl
+LEFT JOIN (
+	SELECT auth_user ppl_login, MAX(timestamp) last_activity
+	FROM session_log
+	GROUP BY auth_user
+) AS last_activity USING (ppl_login)
+LEFT JOIN (
+	SELECT ppl_id, GROUP_CONCAT(tag_name ORDER BY tag_type SEPARATOR '') tags
+	FROM $voxdb.ppl2tag
+	JOIN $voxdb.tag USING (tag_id)
+	WHERE tag_type = 'NIVEAU' OR tag_type = 'LEERJAAR'
+	GROUP BY ppl_id
+) AS tags USING (ppl_id)
+LEFT JOIN passwords ON passwords.auth_user = ppl_login
+ORDER BY last_activity DESC, ppl_type, ppl_surname, ppl_forename, ppl_prefix
 EOQ
 );
 
+/*
+$res = db_query(<<<EOQ
+SELECT ppl_login login,
+	CONCAT(ppl_forename, ' ', ppl_prefix, ' ', ppl_surname) naam, 
+	IFNULL(GROUP_CONCAT(DISTINCT tag_name ORDER BY tag_type SEPARATOR ''), '') tags,
+	CONCAT('<input type="checkbox"', 
+		IF(password_hash IS NULL, ' checked', ''),
+		' value="', ppl.ppl_id, '-', IFNULL(log_id, 'NULL'),
+		'" name="pplpwlog[]">') `genereer wachtwoord`,
+		IFNULL(MAX(session_log.timestamp), 'nog nooit ingelogd') `laatste activiteit`
+FROM $voxdb.ppl
+LEFT JOIN $voxdb.ppl2tag USING (ppl_id)
+LEFT JOIN $voxdb.tag USING (tag_id)
+LEFT JOIN passwords ON passwords.auth_user = ppl_login
+LEFT JOIN session_log ON session_log.auth_user = ppl_login
+WHERE tag_type = 'NIVEAU' OR tag_type = 'LEERJAAR' OR tag_type IS NULL
+GROUP BY ppl.ppl_id
+ORDER BY ppl_type, ppl_surname, ppl_forename, ppl_prefix
+EOQ
+);
+ */
 //exec('pwgen 8 10', $output, $ret);
 //print_r($output);
 //echo($ret);
