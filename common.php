b7ea31f41313dedec0a5570b7dacee249369b924
Rik Snel <rik@snel.it>
2019-02-14 17:36:35 +0100

    make generate_weken_select more flexible

--- common.php
+++ common.php
@@ -1,8 +1,8 @@
 <?
-function generate_weken_select($week_id, $where) {
+function generate_weken_select($week_id, $where, $onchange = "this.form.submit()") {
 	global $voxdb;
 	return db_single_field(<<<EOQ
-SELECT CONCAT('<select onchange="this.form.submit()" name="week_id">', GROUP_CONCAT(CONCAT('<option', IF(week_id = $week_id, ' selected', ''), ' value="', week_id, '">', time_year, 'wk', LPAD(time_week, 2, '0'), IFNULL(CONCAT(' (', week_titel, ')'), ''), '</option>') ORDER BY time_year, time_week), '</select>') FROM $voxdb.weken WHERE $where
+SELECT CONCAT('<select onchange="$onchange" name="week_id">', GROUP_CONCAT(CONCAT('<option', IF(week_id = $week_id, ' selected', ''), ' value="', week_id, '">', time_year, 'wk', LPAD(time_week, 2, '0'), IFNULL(CONCAT(' (', week_titel, ')'), ''), '</option>') ORDER BY time_year, time_week), '</select>') FROM $voxdb.weken WHERE $where
 EOQ
 );
 }
