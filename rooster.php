ac37e982ce1b51dc83349101ac6492f80b462e02
Rik Snel <rik@snel.it>
2019-02-14 17:37:09 +0100

    do not switch roostertarget if student uses selectbutton

--- rooster.php
+++ rooster.php
@@ -21,14 +21,16 @@ Geen lesweken zichtbaar in rooster op dit moment.
 
 $default_week = db_single_field("SELECT CONCAT(time_year, 'wk', LPAD(time_week, 2, '0')) FROM voxdb.weken WHERE week_id = $week_id");
 
-$weken = generate_weken_select($week_id, 'rooster_zichtbaar');
-
+$onsubmit = "this.form.submit()";
 $error = '';
 if (!isset($_GET['q']) || $_GET['q'] == '') {
 	if (check_student() && !isset($_GET['q'])) {
 		$target = db_single_field("SELECT ppl_login FROM $voxdb.ppl WHERE ppl_id = ?", $GLOBALS['session_state']['ppl_id']);
 		$urlencodeq = urlencode($target);
 		$where = ' AND claim.ppl_id = '.$GLOBALS['session_state']['ppl_id'];
+		$onsubmit = <<<EOS
+document.getElementById(\'zoekbox\').disabled = true; this.form.submit();
+EOS;
 	} else {
 		$urlencodeq = '';
 		$target = 'de hele school';
@@ -48,7 +50,7 @@ if (!isset($_GET['q']) || $_GET['q'] == '') {
 	}
 }
 
-
+$weken = generate_weken_select($week_id, 'rooster_zichtbaar', $onsubmit);
 
 $rooster = db_query(<<<EOS
 SELECT time_hour uur, time_day-1 dag, CONCAT(ppl_login, '/', GROUP_CONCAT(DISTINCT subj_abbrev ORDER BY subj_abbrev), ' (<a href="klassenlijst.php?session_guid=$session_guid&amp;time_id=', time_id, '&amp;ppl_id=', avail.ppl_id, '&amp;week_id=$week_id&amp;q=$urlencodeq">', COUNT(DISTINCT claim.ppl_id), '</a>)') 'doc/vak'
@@ -92,7 +94,7 @@ html_start();
 <form method="GET" accept-charset="UTF-8">
 <input type="hidden" name="session_guid" value="<?=$GLOBALS['session_guid']?>">
 <p>Rooster in <?=$weken?> van <?=$target?>.
-<input type="submit" value="Zoek"><input type="text" name="q" placeholder="llnr of docent" autofocus><?=$error?>
+<input type="submit" value="Zoek"><input id="zoekbox" type="text" name="q" placeholder="llnr of docent" autofocus><?=$error?>
 </form>
 
 <? $row = mysqli_fetch_assoc($rooster); ?>
